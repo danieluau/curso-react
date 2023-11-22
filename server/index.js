90aaacce1ebf5103940ca4d58000a3fe15bd34e6
danieluau <daniellealuau@gmail.com>
2023-11-21 21:04:30 -0300

    c e r feitos

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ const express = require('express'); //importando o módulo express
 
 const app = express() //  Você está criando uma instância do aplicativo Express chamada app. 
 //Essa instância representa o seu aplicativo web e é usada para configurar o servidor e definir rotas.
-
+app.use(express.json())
 const db = require('./models')
 
 const postRouter = require('./routes/Posts')
 app.use('/posts', postRouter)
-app.use('/posts', postRouter)
+
 
 db.sequelize.sync().then(()=>{
     app.listen(3001, ()=> {
@@ -17,7 +17,7 @@ db.sequelize.sync().then(()=>{
 
 
 //esse código está sincronizando os modelos definidos com o banco de dados usando o Sequelize e, em seguida, 
-   //iniciando um servidor HTTP na porta 3001 para lidar com as solicitações do cliente. 
+//iniciando um servidor HTTP na porta 3001 para lidar com as solicitações do cliente. 
 
 
 
